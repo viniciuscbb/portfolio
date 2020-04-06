92f72ef5a43e6380276ac769081eedeeb3451c77
Vinicius <vnunes13@gmail.com>
2020-04-06 18:48:55 -0300

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+
+var sendMail = document.getElementById('sendMail').onclick = function () {
+  var nome = document.getElementById('nome').value;
+  var email = document.getElementById('email').value;
+  var assunto = document.getElementById('assunto').value;
+  var mensagem = document.getElementById('mensagem').value;
+
+  if (email == '' || mensagem == '') {
+    alert('Email e mensagem devem ser preenchidos');
+  } else {
+    window.open(
+      `mailto:${email}?subject=${assunto}&body=Olá Vinícius, eu sou o ${nome}!  ${mensagem}`
+    )
+  }
+
+}
